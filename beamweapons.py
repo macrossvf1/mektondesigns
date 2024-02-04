026710b3b3af891d2623ebb9e5a57ea9638026a6
macrossvf1 <macrossvf1@outlook.com>
2024-02-04 17:54:11 -0500

    learning about dictionaries and classes

--- beamweapons.py
+++ beamweapons.py
@@ -2,108 +2,110 @@ import math
 
 class BeamWeapon:
 
-    def __init__(self, damage, accuracy, range, shots, clips, warmuptime,
-                 wideangle,burstvalue,antimissile,antipersonnel,antipersonnelandmissile,
-                 allpurpose, megabeam, longrange, fragile,disruptor, hydro):
-        pass
+    def __init__(self, damage):
+        self.damage = damage
 
-    def beamweapons(args):
-        damage = {
-            'level 1': {'cost': 1.5, 'range': 4},
-            'level 2': {'cost': 3, 'range': 6},
-            'level 3': {'cost': 4.5, 'range': 7},
-            'level 4': {'cost': 6, 'range': 8},
-            'level 5': {'cost': 7.5, 'range': 9},
-            'level 6': {'cost': 9, 'range': 10},
-            'level 7': {'cost': 10.5, 'range': 11},
-            'level 8': {'cost': 12, 'range': 11},
-            'level 9': {'cost': 13.5, 'range': 12},
-            'level 10': {'cost': 15, 'range': 13},
-            'level 11': {'cost': 16.5, 'range': 13},
-            'level 12': {'cost': 18, 'range': 14},
-            'level 13': {'cost': 19.5, 'range': 14},
-            'level 14': {'cost': 21, 'range': 15},
-            'level 15': {'cost': 22.5, 'range': 15},
-            'level 16': {'cost': 24, 'range': 16},
-            'level 17': {'cost': 25.5, 'range': 16},
-            'level 18': {'cost': 27, 'range': 17},
-            'level 19': {'cost': 28.5, 'range': 17},
-            'level 20': {'cost': 30, 'range': 18}
-            }
-
-        # [Range modified, Cost multiplier]
-        range = 
-            [.25,.62],
-            [.5, .75],
-            [.75, .88],
-            [1,1],
-            [1.25,1.12],
-            [1.5, 1.25],
-            [1.75, 1.38],
-            [2, 1.5],
-            [2.5, 1.75],
-            [3, 2]
-
-        # [Accuracy modifier, Cost multiplier]
-        accuracy = 
-            [-2,.6],
-            [-1,.8],
-            [0,.9],
-            [1,1],
-            [2,1.5],
-            [3,2]
-
-        # [Warm-up time, Cost multiplier]
-        warmup_time =  
-            [0,1],
-            [1,.9],
-            [2,.7],
-            [3,.6]
-
-        # [Number of shots, Cost multiplier]
-        shots = 
-            [0,.33],
-            [1,.5],
-            [2,.6],
-            [3,.7],
-            [5,.8],
-            [10,.9],
-            [math.inf,1]
-
-
-        # [Beam angle in degrees, Cost multiplier, Diameter in Hex]
-        wide_angle =
-            [0,2,2],
-            [60,3,1],
-            [180,5,1],
-            [300,7,1],
-            [360,9,1]
-
-        # [Burst value, Cost multiplier]
-        burst_value = 
-            [2,1.5],
-            [3,2],
-            [4,2.5],
-            [5,3],
-            [6,3.5],
-            [7,4],
-            [8,4.5],
-            [math.inf,5]
-
-        # Cost Modifier
-        clip_fed = .9
-
-        # [Range multiplier, Cost multiplier]
-        anti_personnel = [1,1.8]
-        anti_missile = [1,1.8]
-        anti_missile_anti_personnel = [1,1.8]
-        all_purpose = [1,2.6]
-
-        # Cost multipliers
-        fragile = 1
-        long_range = 1.33
-        hydro = .2
-        mega_beam = 10
-        disruptor = 2
-
+    def beamDamage(damage):
+        damage = [
+            {'damage' : 1, 'cost': 1.5, 'range': 4},
+            {'damage' : 2, 'cost': 3, 'range': 6},
+            {'damage' : 3, 'cost': 4.5, 'range': 7},
+            {'damage' : 4, 'cost': 6, 'range': 8},
+            {'damage' : 5, 'cost': 7.5, 'range': 9},
+            {'damage' : 6, 'cost': 9, 'range': 10},
+            {'damage' : 7, 'cost': 10.5, 'range': 11},
+            {'damage' : 8,'cost': 12, 'range': 11},
+            {'damage' : 9, 'cost': 13.5, 'range': 12},
+            {'damage' : 10, 'cost': 15, 'range': 13},
+            {'damage' : 11, 'cost': 16.5, 'range': 13},
+            {'damage' : 12, 'cost': 18, 'range': 14},
+            {'damage' : 13, 'cost': 19.5, 'range': 14},
+            {'damage' : 14, 'cost': 21, 'range': 15},
+            {'damage' : 15, 'cost': 22.5, 'range': 15},
+            {'damage' : 16, 'cost': 24, 'range': 16},
+            {'damage' : 17, 'cost': 25.5, 'range': 16},
+            {'damage' : 18, 'cost': 27, 'range': 17},
+            {'damage' : 19, 'cost': 28.5, 'range': 17},
+            {'damage' : 20, 'cost': 30, 'range': 18}
+            ]
+        print(beam_damage[args])
+        return beam_damage[args]
         
+
+#        # [Range modified, Cost multiplier]
+#        beam_range = [
+#            [.25,.62],
+#            [.5, .75],
+#            [.75, .88],
+#            [1,1],
+#            [1.25,1.12],
+#            [1.5, 1.25],
+#            [1.75, 1.38],
+#            [2, 1.5],
+#            [2.5, 1.75],
+#            [3, 2]
+#            ]
+#
+#        # [Accuracy modifier, Cost multiplier]
+#        beam_accuracy = 
+#            [-2,.6],
+#            [-1,.8],
+#            [0,.9],
+#            [1,1],
+#            [2,1.5],
+#            [3,2]
+#
+#        # [Warm-up time, Cost multiplier]
+#        beam_warmup_time =  
+#            [0,1],
+#            [1,.9],
+#            [2,.7],
+#            [3,.6]
+#
+#        # [Number of shots, Cost multiplier]
+#        beam_shots = 
+#            [0,.33],
+#            [1,.5],
+#            [2,.6],
+#            [3,.7],
+#            [5,.8],
+#            [10,.9],
+#            [math.inf,1]
+#
+#
+#        # [Beam angle in degrees, Cost multiplier, Diameter in Hex]
+#        beam_wide_angle =
+#            [0,2,2],
+#            [60,3,1],
+#            [180,5,1],
+#            [300,7,1],
+#            [360,9,1]
+#
+#        # [Burst value, Cost multiplier]
+#        beam_burst_value = 
+#            [2,1.5],
+#            [3,2],
+#            [4,2.5],
+#            [5,3],
+#            [6,3.5],
+#            [7,4],
+#            [8,4.5],
+#            [math.inf,5]
+#
+#        # Cost Modifier
+#        clip_fed = .9
+#
+#        # [Range multiplier, Cost multiplier]
+#        anti_personnel = [1,1.8]
+#        anti_missile = [1,1.8]
+#        anti_missile_anti_personnel = [1,1.8]
+#        all_purpose = [1,2.6]
+#
+#        # Cost multipliers
+#        fragile = 1
+#        long_range = 1.33
+#        hydro = .2
+#        mega_beam = 10
+#        disruptor = 2
+#
+#        
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os, re
 from beamweapons import BeamWeapon
-from frame import Frame 
+#from frame import Frame 
 
-#big_beam_gun = BeamWeapon()
 
 class_list = ['superlight', 'lightweight', 'striker', 'medium_striker', 'heavy_striker', 'mediumweight',
                   'light_heavy', 'medium_heavy', 'armored_heavy', 'super_heavy', 'mega_heavy']
@@ -11,6 +10,9 @@ ram_armor_class_list = ['level 1', 'level 2', 'level 3', 'level 4']
 
 user_name = ""
 user_CP = ""
+
+weapon_inventory = []
+missile_inventory = {}
 
 
 def getInitialData():
@@ -25,6 +27,42 @@ def getInitialData():
     except ValueError:
         input("That's not an acceptable value.")
         
+def buildFrame():
+    pass
+
+def buildBeamWeapon():
+    # Gather user input to send to BeamWeapon class to create an instance of that weapon.
+    global weapon_inventory
+    beam_weapon = {}    # Create a dictionary of the new weapon's stats
+    beam_name = ""
+    beam_damage = ""    # Get table values from beamweapons
+    beam_range = ""
+    beam_accuracy = ""
+    beam_shots = ""
+    beam_burst_value = ""
+    beam_warm_up_time = ""
+    beam_wide_angle = ""
+    beam_clipfed = False
+    beam_anti_missile = False
+    beam_anti_personnel = False
+    beam_anti_missile_personnel = False
+    beam_all_purpose = False
+    beam_fragile = False
+    beam_long_range = False
+    beam_hydro = False
+    beam_disruptor = False
+
+    beam_name = input("Name the new beam weapon.\n")
+    beam_weapon['Beam Weapon'] = beam_name
+
+    beam_damage = input("How much damage will the beam weapon do? (1-20)\n")
+    #beam_weapon['Beam Damage'] = beam_damage
+   
+    new_beam_weapon = BeamWeapon(beam_damage)
+    print(new_beam_weapon.beamDamage())
+
+    # Update Weapons Inventory with the new Beam Weapon.
+    weapon_inventory.append(beam_weapon)
 
 if __name__ == "__main__":
     # Build processes
@@ -32,20 +70,21 @@ if __name__ == "__main__":
     #buildFrame()
     buildBeamWeapon()
 
-    # Summary
-    print(
-        "Name: " + user_name + "\n",
-        "CP: " + str(user_CP) + "\n\n",
-        "Current Build \n",
-        "Torso: " + str(user_torso) + "\n",
-        "Head: " + str(user_head) + "\n",
-        "Arm, Right: " + str(user_arm_right) + "\n",
-        "Arm, Left: " + str(user_arm_left) + "\n",
-        "Leg, Right: " + str(user_leg_right) + "\n",
-        "Leg, Left: " + str(user_leg_left) + "\n",
-        "Wing: " + str(user_wing) + "\n",
-        "Tail: " + str(user_tail) + "\n",
-        "Armor: " + str(user_armor) + "\n",
-        "Armor Type: " + str(user_armortype) + "\n",
-        "RAM Armor: " + str(user_ram_armor) + "\n",
-    )
+
+# Summary
+#    print(
+#        "Name: " + user_name + "\n",
+#        "CP: " + str(user_CP) + "\n\n",
+#        "Current Build \n",
+#        "Torso: " + str(user_torso) + "\n",
+#        "Head: " + str(user_head) + "\n",
+#        "Arm, Right: " + str(user_arm_right) + "\n",
+#        "Arm, Left: " + str(user_arm_left) + "\n",
+#        "Leg, Right: " + str(user_leg_right) + "\n",
+#        "Leg, Left: " + str(user_leg_left) + "\n",
+#        "Wing: " + str(user_wing) + "\n",
+#        "Tail: " + str(user_tail) + "\n",
+#        "Armor: " + str(user_armor) + "\n",
+#        "Armor Type: " + str(user_armortype) + "\n",
+#        "RAM Armor: " + str(user_ram_armor) + "\n",
+#    )
